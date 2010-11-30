e08ea54c35e2ce9e57d7568964a7622fe72cd3c2
Rafael George <george.rafael@gmail.com>
2010-11-30 18:12:38 -0400

    added fixtures in json format

--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -4,7 +4,18 @@ describe "Ticketmaster::Provider::Bitbucket::Ticket" do
   before(:all) do 
     headers = {}
     ActiveResource::HttpMock.respond_to do |mock|
+        mock.get "/1.0/repositories/foo/test-repo/issues", headers, fixture_for('projects/issues'), 200
     end
+    @project_id = 'test-repo'
   end
-  ZZ"
+
+  before(:each) do 
+      @ticketmaster = TicketMaster.new(:bitbucket, :username => 'foo', :password => '000000')
+      @project = @ticketmaster.project(@project_id)
+      @klass = TicketMaster::Provider::Bitbucket::Ticket
+  end
+
+  it "should be able to load all tickets" do 
+      @project.tickets.should be_an_instance_of(Array)
+  end
 end
