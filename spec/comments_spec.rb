48846994089e2f59127eb3ba0928d54f414b475e
Rafael George <george.rafael@gmail.com>
2010-11-30 13:16:10 +0000

    remove expectations from comment and ticket specs

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Bitbucket::Comment" do
-  it "should have specs for comments"
 end
--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Ticketmaster::Provider::Yoursystem::Ticket" do
-  it "should have specs for tickets"
+describe "Ticketmaster::Provider::Bitbucket::Ticket" do
 end
