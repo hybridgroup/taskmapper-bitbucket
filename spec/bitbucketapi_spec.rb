16d479367241fea9c5d6da0767f0faa6ef84ae31
Rafael George <george.rafael@gmail.com>
2010-12-07 17:46:07 -0400

    adding specs for bitbucketapi - added spec for repository class - added expectations for bitbucketapi user class - made some refactor to not repeat myself - adding expectation for issues

--- spec/bitbucketapi_spec.rb
+++ spec/bitbucketapi_spec.rb
@@ -3,15 +3,33 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "BitbucketAPI" do
 
     before(:all) do
-        headers = {"AuthorizationBasic" => " Zm9vOjAwMDAwMA==", "Accept" => "application/json"}
+        headers = {'Authorization' => 'Basic Zm9vOjAwMDAwMA==', 'Accept' => 'application/json'}
+        @repo_response = { :prefix => 1 }
         ActiveResource::HttpMock.respond_to do |mock|
-            mock.get "/1.0/users/cored/", headers, [], 200 
+            mock.get "/1.0/users/cored/", headers, fixture_for('projects'), 200 
         end
     end
 
     describe "BitbucketAPI::Repository" do 
         it "should extract a repository for a giving user" do 
-            @repository = BitbucketAPI::Repository.find(:all, :username => 'cored')
+            BitbucketAPI::Repository.find(:all, :username => 'cored').should be_an_instance_of(Array)
+        end
+
+        it "should have an array of issues" do 
+            BitbucketAPI::Repository.find(:all, :username => 'cored').issues.should be_an_instance_of(Array)
+        end
+    end
+
+    describe "BitbucketAPI::User" do 
+        before(:each) do
+            @call = BitbucketAPI::User.find('cored')
+        end
+        it "should extract user info from a givin user" do 
+            @call.user.username.should == 'cored'
+        end
+
+        it "should have a list of repositories" do 
+            @call.repositories.should be_an_instance_of(Array)
         end
     end
 end
