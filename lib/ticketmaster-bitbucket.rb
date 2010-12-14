3cf60ac9f29e71526ca1d10f2dea7722421ef9ed
Rafael George <george.rafael@gmail.com>
2010-12-14 17:39:35 -0400

    removed require for the bitbucket-api

--- lib/ticketmaster-bitbucket.rb
+++ lib/ticketmaster-bitbucket.rb
@@ -1,5 +1,3 @@
-require File.dirname(__FILE__) + '/bitbucket/bitbucket-api.rb'
-
 %w{ bitbucket ticket project comment }.each do |f|
   require File.dirname(__FILE__) + '/provider/' + f + '.rb';
 end
